262836fa79f717cde990cd5f3fa5ef6dc8ffa3f1
Alexand3r12345 <alexander.angman@elev.ga.ntig.se>
2026-02-03 13:33:35 +0100

    2026-02-03

--- test.py
+++ test.py
@@ -20,38 +20,36 @@ while fel < max_fel and "_" in guessed_word:
     print("gissade bokstäver:", ", ".join(used_letters))
 
 
-try:
-     guess = input("Gissa ditt ord").lower()
+    try:
+        guess = input("Gissa en bokstav").lower()
 
-     if len(guess) != 1:
-        raise ValueError("du måste gissa en bokstav")
+        if len(guess) != 1:
+            raise ValueError("du måste gissa en bokstav")
+     
+        if guess in used_letters:
+            raise ValueError("du har redan gissat den bokstaven")
+    
+        used_letters.append(guess)
 
-except ValueError as f:
-    print("fel", f)
-        
-
-
-if guess in play_word:
-    for i, letter in enumerate(play_word):
-        if letter == guess:
+        if guess in play_word:
+             for i, letter in enumerate(play_word):
+                  if letter == guess:
                     guessed_word[i] = guess
-        print("Rätt!")
+                    print("rätt")
+        else:
+            fel += 1
+            print("fel, antal fel:", fel, "av", max_fel)
+        
+    except ValueError as f:
+            print("fel", f)
+        
+
+if "_" not in guessed_word:
+    print("yay du vann ordet var:", word)
+else:
+    print("tyvärr du förlorade ordet var:", word)
 
 
-    
-   
-    if guess == word:
-        print("Du vann")
-        break
-        
-    
-    else:
-        fel += 1
-        print("Fel, antal fel,", fel, "av", max_fel)
-        
-        if fel == 6:
-            print("Du förlorade")
-            break
  
 
 
